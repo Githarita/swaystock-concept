e26d2421f2e6b372910af360b5dd576ad20703ac
Emmanuel Oladimeji Adeyemi <46575938+yemicodes@users.noreply.github.com>
2022-06-18 12:31:44 +0300

    another update

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -631,7 +631,7 @@ function Homepage() {
       {bottomnav && (
         <>
           <div className="bottomnav_modalOverlay" />
-          <div className="bottomnav_modalWrap" onClick={homebtnIconClick}>
+          <div className="bottomnav_modalWrap">
             <div className="bottomnav">
               <div className="bottomnav_item">
                 <p>Home</p>
@@ -689,6 +689,21 @@ function Homepage() {
                     src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
                     alt=""
                   />
+                </div>
+              </div>
+              <div className="bottomnav_close">
+                <div className="closebottomnav_item" onClick={homebtnIconClick}>
+                  <svg
+                    viewBox="0 0 15 15"
+                    className="closebottomnav_icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z"
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
                 </div>
               </div>
             </div>
