7d1ed6f8cd08a8c418cf36afc8bdfac3599b6d90
Emmanuel Oladimeji Adeyemi <46575938+yemicodes@users.noreply.github.com>
2022-06-26 09:57:05 +0300

    adding contents

--- src/Pages/Categoryproducts/Categoryproducts.js
+++ src/Pages/Categoryproducts/Categoryproducts.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import "./Categoryproducts.css";
+import sneakercatpageimg from "../../assets/sneakercatimg.png";
 
 function Categoryproducts() {
   let { categoryId } = useParams();
@@ -9,7 +10,20 @@ function Categoryproducts() {
   return (
     <>
       <Header />
-      <div className="">Categoryproducts {categoryId}</div>
+      <header className="categoryproducts_header">
+        <div className="categoryproducts_header-item">
+          <div className="categoryproducts_header-item-text">
+            <h1>{categoryId}</h1>
+            <div className="">
+              <p>Oh. So. Comfy.</p>
+              <p>Love the power. Love the price.</p>
+              <p>Check back later fo more available products.</p>
+              <p>Suggest a product</p>
+            </div>
+          </div>
+          <img src={sneakercatpageimg} alt="" />
+        </div>
+      </header>
     </>
   );
 }
