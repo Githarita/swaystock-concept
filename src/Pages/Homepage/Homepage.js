b6f1ce0403220a9afb5a2aa848a93936afc00a2a
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-06-29 22:12:17 +0300

    Update Homepage.js

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -714,7 +714,7 @@ function Homepage() {
             </div>
             <div className="bottomnav_item">
               {/* <p>Nofication</p> */}
-              <div className="bottonav_icon-cnt" onClick={homebtnIconClick}>
+              <div className="bottonav_icon-cnt">
                 <svg
                   className="bottomnav__icon"
                   viewBox="0 0 15 15"
@@ -731,7 +731,7 @@ function Homepage() {
 
             <div className="bottomnav_item">
               {/* <p>Profile</p> */}
-              <div className="bottonav_icon-cnt" onClick={homebtnIconClick}>
+              <div className="bottonav_icon-cnt">
                 <img
                   className="bottomnav__image"
                   src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
