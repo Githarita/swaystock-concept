582922100ca494aef07714c96b6590bb7db2e8d6
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-07-09 13:51:50 +0300

    update on form

--- src/Pages/BuyingInfopage/BuyingInfopage.js
+++ src/Pages/BuyingInfopage/BuyingInfopage.js
@@ -45,14 +45,30 @@ function BuyingInfopage() {
             <div className="buying-info-container-formitem">
               <h5 className="bicftitle">Shipping Information</h5>
               <p className="bicfbtn">Add Shipping Information</p>
-
-              <div className="omrs-input-group">
-                <label className="omrs-input-underlined">
-                  <input type="text" />
-                  <span className="omrs-input-label">Full Legal Name</span>
-                </label>
+              <div className="omrs-form">
+                <div className="omrs-input-group">
+                  <label className="omrs-input-underlined">
+                    <input type="text" />
+                    <span className="omrs-input-label">Full Legal Name</span>
+                  </label>
+                </div>
+                <div className="omrs-input-group">
+                  <label className="omrs-input-underlined">
+                    <input type="text" />
+                    <span className="omrs-input-label">Street Address</span>
+                  </label>
+                </div>
+                <div className="omrs-input-group">
+                  <label className="omrs-input-underlined">
+                    <input type="text" />
+                    <span className="omrs-input-label">
+                      Street Address2 (Optional)
+                    </span>
+                  </label>
+                </div>
               </div>
             </div>
+
             <div className="buying-info-container-formitem">
               <h5 className="bicftitle">Billing Information</h5>
               <p className="bicfbtn">Add Billing Address</p>
@@ -63,7 +79,10 @@ function BuyingInfopage() {
             </div>
 
             <div className="bicfremove">
-              <div className="bicfremoveitem" onClick={() => setActive("addbtn")}>
+              <div
+                className="bicfremoveitem"
+                onClick={() => setActive("addbtn")}
+              >
                 <svg viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg">
                   <path
                     d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
@@ -74,7 +93,10 @@ function BuyingInfopage() {
                 <p>Discard</p>
               </div>
 
-              <div className="bicfremoveitem" onClick={() => setActive("addbtn")}>
+              <div
+                className="bicfremoveitem"
+                onClick={() => setActive("addbtn")}
+              >
                 <p>Save</p>
                 <svg viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg">
                   <path
