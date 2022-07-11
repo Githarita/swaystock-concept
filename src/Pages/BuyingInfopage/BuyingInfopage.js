85d0dc8926e0a8a9538947b724a74bb14d3186f3
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-07-11 14:40:39 +0300

    Update BuyingInfopage.js

--- src/Pages/BuyingInfopage/BuyingInfopage.js
+++ src/Pages/BuyingInfopage/BuyingInfopage.js
@@ -11,8 +11,6 @@ function BuyingInfopage() {
   const [lgaShow, setLgaShow] = useState(false);
   const [selectedState, setSelectedState] = useState("State");
   const [selectedBillState, setSelectedBillState] = useState("State");
-  const [displayam, setDisplayam] = useState("");
-  const [displayamlga, setDisplayamlga] = useState("");
   const [selectedLga, setSelectedLga] = useState("Local Government Area");
   const [selectedBillLga, setSelectedBillLga] = useState(
     "Local Government Area"
@@ -385,8 +383,79 @@ function BuyingInfopage() {
 
             {showPaymentMethod && (
               <div className="buying-info-container-formitem ">
-                <h5 className="bicftitle">Payment</h5>
-                <p className="bicfbtn">Add Payment Method</p>
+                <div>
+                  <h5 className="bicftitle">Payment</h5>
+                  {showaddpayment && (
+                    <p
+                      className="bicfbtn"
+                      onClick={() => {
+                        setShowInfo("paymentmethod");
+                        setPaymentMethod(true);
+                        setShippingInformation(false);
+                        setBillingAddress(false);
+                        setShowaddpayment(false);
+                      }}
+                    >
+                      Add Payment Method
+                    </p>
+                  )}
+                </div>
+                {showInfo === "paymentmethod" && (
+                  <form autoComplete="on" className="omrs-form">
+                    <h1>This is the payment stage</h1>
+                    <div className="bicfremove">
+                      <div
+                        className="bicfremoveitem"
+                        onClick={() => {
+                          setBillingAddress(true);
+                          setPaymentMethod(true);
+                          setShippingInformation(true);
+                          setShowInfo();
+                          setShowaddship(true);
+                          setShowaddbill(true);
+                          setShowaddpayment(true);
+                        }}
+                      >
+                        <svg
+                          viewBox="0 0 15 15"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M11.7816 4.03157C12.0062 3.80702 12.0062 3.44295 11.7816 3.2184C11.5571 2.99385 11.193 2.99385 10.9685 3.2184L7.50005 6.68682L4.03164 3.2184C3.80708 2.99385 3.44301 2.99385 3.21846 3.2184C2.99391 3.44295 2.99391 3.80702 3.21846 4.03157L6.68688 7.49999L3.21846 10.9684C2.99391 11.193 2.99391 11.557 3.21846 11.7816C3.44301 12.0061 3.80708 12.0061 4.03164 11.7816L7.50005 8.31316L10.9685 11.7816C11.193 12.0061 11.5571 12.0061 11.7816 11.7816C12.0062 11.557 12.0062 11.193 11.7816 10.9684L8.31322 7.49999L11.7816 4.03157Z"
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                          ></path>
+                        </svg>
+                        <p>Discard</p>
+                      </div>
+
+                      <div
+                        className="bicfremoveitem"
+                        onClick={() => {
+                          setShowaddship(true);
+                          setBillingAddress(true);
+                          setPaymentMethod(true);
+                          setShippingInformation(true);
+                          setShowInfo(false);
+                          setShowaddbill(true);
+                          setShowaddpayment(true);
+                        }}
+                      >
+                        <p>Save</p>
+                        <svg
+                          viewBox="0 0 15 15"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z"
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                  </form>
+                )}
               </div>
             )}
           </div>
@@ -404,7 +473,6 @@ function BuyingInfopage() {
                   key={key}
                   onClick={() => {
                     setSelectedState(state.name);
-                    setDisplayam("none");
                     setStateShow(false);
                   }}
                 >
@@ -426,7 +494,6 @@ function BuyingInfopage() {
                   key={key}
                   onClick={() => {
                     setSelectedLga(lg.name);
-                    setDisplayamlga("none");
                     setLgaShow(false);
                     setStateShow(false);
                   }}
@@ -449,7 +516,6 @@ function BuyingInfopage() {
                   key={key}
                   onClick={() => {
                     setSelectedBillState(state.name);
-                    setDisplayam("none");
                     setStateShow(false);
                   }}
                 >
@@ -471,7 +537,6 @@ function BuyingInfopage() {
                   key={key}
                   onClick={() => {
                     setSelectedBillLga(lg.name);
-                    setDisplayamlga("none");
                     setLgaShow(false);
                     setStateShow(false);
                   }}
