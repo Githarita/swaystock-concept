097a12b624d989c2b304c28c991baadd2c1d447d
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-07-20 16:54:12 +0400

    Update Howitworkspage.js

--- src/Pages/Howitworkspage/Howitworkspage.js
+++ src/Pages/Howitworkspage/Howitworkspage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Howitworkspage.css";
 
 function Howitworkspage() {
@@ -23,37 +23,79 @@ function Howitworkspage() {
         <p className="settings-top-text">How it Works</p>
       </header>
       <div className="buying-info-containerr">
-        <div className="buying-info-container-form">
-          <div className="howitworkscnt">
-            <div className="howitworkstop">
-              <div className="howitworkstopitem">
-                <h1>BUY</h1>
-              </div>
-              <div className="howitworkstopitem">
-                <h1>SELL</h1>
-              </div>
+        <div className="howitworkscnt">
+          <div className="howitworkstop">
+            <div className="howitworkstopitem">
+              <h1>BUY</h1>
             </div>
-            <div className="howitworksbottom">
-              <div className="howitworksbottombuy">
-                <p>Place A Bid Or Buy Now</p>
-                <p>We Authenticate</p>
-                <p>We Ship To You</p>
+            <div className="howitworkstopitem">
+              <h1>SELL</h1>
+            </div>
+          </div>
+          <div className="howitworksbottom">
+            <div className="howitworksbottombuy">
+              <p>Place A Bid Or Buy Now</p>
+              <p>We Authenticate</p>
+              <p>We Ship To You</p>
 
-                <div className="hiwbb">
-                  <h2>Buying On SwayStock</h2>
-                  <p>
-                    We don't determine the price, you do. As a live marketplace,
-                    SwayStock empowers you to Bid and Buy at real-time prices
-                    that reflect the current demand
-                  </p>
-                </div>
+              <div className="hiwbb">
+                <h2>Buying On SwayStock</h2>
+                <p>
+                  We don't determine the price, you do. As a live marketplace,
+                  SwayStock empowers you to Bid and Buy at real-time prices that
+                  reflect the current demand
+                </p>
+              </div>
 
-                <div className="hiwbbnameprice">
-                  <h2>Name Your Price</h2>
+              <div className="hiwbbnameprice">
+                <h2>Name Your Price</h2>
+                <p>
+                  Buy Now at the lowest Ask, or place a lower Bid at the price
+                  you want to pay. We will send you immediate updates as prices
+                  move on your Bids. Bids can be renewed or let go when they're
+                  about to expire, and you'll be notified ahead of time
+                </p>
+              </div>
+
+              <div className="hiwbbnameprice">
+                <h2>Browse Our Nigeria Marketplace</h2>
+                <p>
+                  Discover and shop the products you love from around the world.
+                  Doesn't matter if it's a recently sold out console or an
+                  internationally exclusive product, you can always find them
+                  available for purchase on our marketplace.
+                </p>
+              </div>
+
+              <div className="hiwbbnameprice">
+                <h2>Make Data-Driven Purchases</h2>
+                <p>
+                  Stay informed with powerful data to help you make the right
+                  purchases. View analytics like historical prices and
+                  time-stamped trades for every item to help you shop smarter.
+                </p>
+              </div>
+
+              <div className="hiwbbnameprice">
+                <h2>Enjoy Guaranteed Authenticity</h2>
+                <p>
+                  Our global team of authenticators inspect all items by hand.
+                  They'll verify and make sure every item you buy is completely
+                  authentic and brand new before it arrives at your door.
+                </p>
+
+                <Link to="/">Learn More about Authenticity</Link>
+              </div>
+
+              <div className="stepsonsuccess">
+                <h1>Our Steps For Success</h1>
+                <div className="stepsonsuccess-item">
+                  <h2>Bid or Buy</h2>
                   <p>
-                    Buy Now at the lowest Ask, or place a lower Bid at the price
-                    you want to pay. We will send you immediate updates as
-                    prices move on your Bids. Bids can be renewed or
+                    On SwayStock there are two ways to buy: Bid at the price of
+                    your choice and watch our dynamic marketplace come to life,
+                    or Buy Now at the lowest Ask to secure your item without any
+                    gamesmanship.
                   </p>
                 </div>
               </div>
