945bb7c11e74472e9283e2ac4539548b72923f31
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-07-20 14:52:52 +0300

    updating

--- src/Pages/Profilepage/Profilepage.js
+++ src/Pages/Profilepage/Profilepage.js
@@ -23,6 +23,17 @@ function Profilepage() {
         <p className="settings-top-text">Profile</p>
       </header>
 
+      <div className="buying-info-container">
+        <div className="profilepage-cnt">
+          <div className="primage">
+          <img
+              src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+
     </>
   )
 }
