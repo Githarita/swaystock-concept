3209e6723f01b3c02d43d07a712ae3ae411974f4
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-07-21 23:55:35 +0300

    udpate

--- src/Pages/Productpage/Productpage.js
+++ src/Pages/Productpage/Productpage.js
@@ -50,112 +50,17 @@ function Productpage() {
           <img src={sneaker5} alt="sneaker5" />
         </div>
 
-        <div className="productpage-infomix">
-          <div className="productpage-infomixitem1">
-            <div className="productpage-info">
-              <p className="productpage-infocat">Sneakers</p>
-              <h1 className="productpage-infotitle">{productId}</h1>
-            </div>
-            <div className="productpage-lowestask">
-              <p className="productpage-lowestask-subtext">Lowest Ask</p>
-              <h1 className="productpage-lowestask-price">N120,000</h1>
-            </div>
-          </div>
-          <div className="productpage-infomixitem2">
-            <div className="productpage-infomixitem2b">
-                 <svg
-              className="productpage-infomixitem2-icon"
-              viewBox="0 0 15 15"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM1.82707 7.49972C1.82707 4.36671 4.36689 1.82689 7.49991 1.82689C10.6329 1.82689 13.1727 4.36671 13.1727 7.49972C13.1727 10.6327 10.6329 13.1726 7.49991 13.1726C4.36689 13.1726 1.82707 10.6327 1.82707 7.49972ZM7.50003 4C7.77617 4 8.00003 4.22386 8.00003 4.5V7H10.5C10.7762 7 11 7.22386 11 7.5C11 7.77614 10.7762 8 10.5 8H8.00003V10.5C8.00003 10.7761 7.77617 11 7.50003 11C7.22389 11 7.00003 10.7761 7.00003 10.5V8H4.50003C4.22389 8 4.00003 7.77614 4.00003 7.5C4.00003 7.22386 4.22389 7 4.50003 7H7.00003V4.5C7.00003 4.22386 7.22389 4 7.50003 4Z"
-                fill="currentColor"
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-            </div>
-           
-            <div className="productpage-category-item">
-              <p>Market Data</p>
-            </div>
-          </div>
-        </div>
+       <div className="productpage-itemname">
+        <h1>{productId}</h1>
+       </div>
 
-        {/* <div className="productpage-notifi">
-          <svg
-            className="productpage-notifi-icon"
-            viewBox="0 0 15 15"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M8.60124 1.25086C8.60124 1.75459 8.26278 2.17927 7.80087 2.30989C10.1459 2.4647 12 4.41582 12 6.79999V10.25C12 11.0563 12.0329 11.7074 12.7236 12.0528C12.931 12.1565 13.0399 12.3892 12.9866 12.6149C12.9333 12.8406 12.7319 13 12.5 13H8.16144C8.36904 13.1832 8.49997 13.4513 8.49997 13.75C8.49997 14.3023 8.05226 14.75 7.49997 14.75C6.94769 14.75 6.49997 14.3023 6.49997 13.75C6.49997 13.4513 6.63091 13.1832 6.83851 13H2.49999C2.2681 13 2.06664 12.8406 2.01336 12.6149C1.96009 12.3892 2.06897 12.1565 2.27638 12.0528C2.96708 11.7074 2.99999 11.0563 2.99999 10.25V6.79999C2.99999 4.41537 4.85481 2.46396 7.20042 2.3098C6.73867 2.17908 6.40036 1.75448 6.40036 1.25086C6.40036 0.643104 6.89304 0.150421 7.5008 0.150421C8.10855 0.150421 8.60124 0.643104 8.60124 1.25086ZM7.49999 3.29999C5.56699 3.29999 3.99999 4.86699 3.99999 6.79999V10.25L4.00002 10.3009C4.0005 10.7463 4.00121 11.4084 3.69929 12H11.3007C10.9988 11.4084 10.9995 10.7463 11 10.3009L11 10.25V6.79999C11 4.86699 9.43299 3.29999 7.49999 3.29999Z"
-              
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-          <h2 className="productpage-notifitext">7 Left for this item</h2>
-        </div> */}
+       
 
-        <div
-          className="productpage-category-itemm"
-        >
-          <p>Description</p>
-        </div>
-        <div className="productpage-desciption">
-          <p>
-            From the school-spirited College Colors Program to the vibrant Nike
-            CO.JP collection, Nike Dunks have seen many colorways since the
-            design's inception in 1985. But with each new colorway, the Dunk's
-            classic color-blocking has remained in some capacity. Nike put its
-            timeless color-blocking to work with the Nike Dunk Low Retro White
-            Black.
-          </p>
+       
+        
 
-          <p>
-            The Upper Nike Dunk Low Retro White Black is constructed of white
-            leather with black leather overlays and Swooshes. Classic Nike
-            branding is featured on the nylon tongue, nodding to traditional
-            Dunk design elements. A white midsole and black outsole completes
-            the desing.
-          </p>
-
-          <p>
-            The Nike Dunk Low Retro White Black released in January of 2021 and
-            retailed for N90,000.
-          </p>
-        </div>
-
-        <div
-          className="productpage-category-itemm"
-        >
-          <p>Product Details</p>
-        </div>
-        <div className="productpage-details">
-          <div className="productpage-details-item">
-            <p className="pditext">Category</p>
-            <p className="pdians">Sneakers</p>
-          </div>
-          <div className="productpage-details-item">
-            <p className="pditext">Conditon</p>
-            <p className="pdians">New</p>
-          </div>
-          <div className="productpage-details-item">
-            <p className="pditext">Authencity</p>
-            <p className="pdians">100%</p>
-          </div>
-          <div className="productpage-details-item">
-            <p className="pditext">Retail Price</p>
-            <p className="pdians">N90,000</p>
-          </div>
-          <div className="productpage-details-item">
-            <p className="pditext">Realease Date</p>
-            <p className="pdians">02 June 2021</p>
-          </div>
-        </div>
+       
+        
       </div>
       {searchmodal && (
         <div className="searchproductmodal">
